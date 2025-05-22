07d3dcfad64502849d2e976c7ad87fd0e973e563
uwe77 <uwe90711@gmail.com>
2025-05-23 00:33:15 +0800

    feat(environment): add scripts for setting ROS_IP and ROS_MASTER_URI
    Added new scripts to configure ROS_IP and ROS_MASTER_URI. The
    'set_ros_ip.sh' script sets the ROS_IP environment variable based
    on user input or defaults to a prompt. The 'set_ros_master.sh'
    script configures the ROS_MASTER_URI, using a provided hostname
    or the system's hostname if none is given. The main environment
    script has been updated to source these new scripts accordingly.

---
+++ environment.sh
@@ -0,0 +1,18 @@
+# source /opt/ros/noetic/setup.bash
+source $HOME/local_collision_avoidance/catkin_ws/devel/setup.bash
+
+if [ $# -gt 0 ]; then
+	export ROS_MASTER_IP=$1
+    echo "ROS_MASTER_IP set to $ROS_MASTER_IP"
+    source set_ros_master.sh $ROS_MASTER_IP
+else
+    source set_ros_master.sh 127.0.0.1
+fi
+
+if [ $# -gt 0 ]; then
+	export ROS_IP=$2
+    echo "ROS_IP set to $ROS_IP"
+    source set_ros_ip.sh $ROS_IP
+else
+    source set_ros_ip.sh 127.0.0.1
+fi
