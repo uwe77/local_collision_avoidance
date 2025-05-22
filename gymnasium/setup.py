396cb1b483917d904377954beda268c059dbe12c
uwe77 <uwe90711@gmail.com>
2025-05-23 00:36:08 +0800

    feat(setup): rename package to gymnasium-cus
    Updated the package name in setup.py from 'gymnasium_vrx' to
    'gymnasium-cus' to better reflect its purpose. Additionally,
    added a new script 'install_gymnasium.sh' for easier installation
    of the gymnasium package.

--- gymnasium/setup.py
+++ gymnasium/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='gymnasium_vrx',
+setup(name='gymnasium-cus',
       version='0.0.1',
       install_requires=['gymnasium']  # And any other dependencies foo needs
 )
